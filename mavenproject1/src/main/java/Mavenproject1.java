bd1b11357ea16c91c5527525678e505a80211baa
SubashMM18 <mmsubash2004@gmail.com>
2025-02-08 11:02:01 +0530

    Add files via upload

---
+++ mavenproject1/src/main/java/Mavenproject1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+/**
+ *
+ * @author IT20
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Addition of two numbers");
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a=scanner.nextInt();
+        System.out.println("Enter b:");
+        int b=scanner.nextInt();
+        int c=a+b;
+        System.out.println("Sum:"+c);
+    }
+}
+
